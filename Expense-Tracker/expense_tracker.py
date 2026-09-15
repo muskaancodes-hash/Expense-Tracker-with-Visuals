04ff30ddb50a245d29618ef6f98550f458da747d
muskaancodes-hash <muskaan.kumari7908@gmail.com>
2026-09-15 12:51:30 +0530

    Add files via upload
    Performed category-wise expense analysis using Pandas groupby() and identified the category with the highest total spending.

--- Expense-Tracker/expense_tracker.py
+++ Expense-Tracker/expense_tracker.py
@@ -24,3 +24,15 @@ print(df.isnull().sum())
 # Total expenses
 print("\nTotal Expenses:")
 print("₹", df["Amount"].sum())
+# Category-wise expense analysis
+print("\n===== CATEGORY-WISE EXPENSES =====")
+
+category_expenses = df.groupby("Category")["Amount"].sum()
+
+print(category_expenses)
+
+highest_category = category_expenses.idxmax()
+highest_amount = category_expenses.max()
+
+print("\nHighest Spending Category:")
+print(highest_category, "→ ₹", highest_amount)
